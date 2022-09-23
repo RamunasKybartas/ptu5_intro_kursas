3c61b87493c8fd8d7e83464f537f7b3f9c2932f3
RamunasKybartas <kybartas.ramunas@gmail.com>
2022-09-23 14:10:33 +0300

    added functions files

---
+++ funkcijos.py
@@ -0,0 +1,6 @@
+# Sukurti funkcijas, kurios:
+
+# Gražintų visų paduotų skaičių sumą (su *args argumentu)
+# Gražintų paduoto skaičiaus šaknį (panaudoti math.sqrt())
+# Gražintų paduoto sakinio simbolių kiekį (su len())
+# Gražintų rezultatą, skaičių x padalinus iš y
